3e524fcd1169aab30cba737e6c13411a29a11f8e
samobeng14Dev <obengsamuel270@gmail.com>
2024-10-15 16:42:26 +0000

    updated app

--- time_finder/time_finder_app/lib/main.dart
+++ time_finder/time_finder_app/lib/main.dart
@@ -14,32 +14,38 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Text('Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Center(
+          child: Text("Hello Flutter",style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily:'IndieFlower',
+          ),),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Define the action when the button is pressed
+            print('FloatingActionButton clicked');
+          },
+          child: Icon(Icons.add), 
+          backgroundColor: Colors.red[600],
+        ),
+      ),
     );
   }
 }
-
