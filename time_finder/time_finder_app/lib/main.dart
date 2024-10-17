c431e5d7eca494cec2a440d2ce30d6070fa60cf1
samobeng14Dev <obengsamuel270@gmail.com>
2024-10-17 12:41:21 +0000

    updated file

--- time_finder/time_finder_app/lib/main.dart
+++ time_finder/time_finder_app/lib/main.dart
@@ -20,7 +20,7 @@ class Home extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.grey[900],
         appBar: AppBar(
-          title: Text('S card', style: TextStyle(color: Colors.white)),
+          title: Text('S NINJA ID', style: TextStyle(color: Colors.white)),
           centerTitle: true,
           backgroundColor: Colors.grey[850],
           elevation: 0.0,
@@ -29,7 +29,41 @@ class Home extends StatelessWidget {
           padding: EdgeInsets.fromLTRB(40.0, 40.0, 30.0, 0.0),
           child: Column(
             children: <Widget>[
-              
+              Text(
+                'NAME',
+                style: TextStyle(color: Colors.grey, letterSpacing: 1.0),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Kwame Afriyie',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 1.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'CURRENT LEVEL',
+                style: TextStyle(color: Colors.grey, letterSpacing: 1.0),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                '8',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 1.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 30.0,
+              )
+            
             ],
           ),
         ),
