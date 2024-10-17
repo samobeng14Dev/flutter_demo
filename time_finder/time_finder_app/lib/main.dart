b39276f270f29d67216436e22ab2d36af7d201fe
samobeng14Dev <obengsamuel270@gmail.com>
2024-10-17 12:17:09 +0000

    updated file

--- time_finder/time_finder_app/lib/main.dart
+++ time_finder/time_finder_app/lib/main.dart
@@ -17,27 +17,21 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home: Scaffold(
+        backgroundColor: Colors.grey[900],
         appBar: AppBar(
-          title: Text('My First App'),
+          title: Text('S card', style: TextStyle(color: Colors.white)),
           centerTitle: true,
-          backgroundColor: Colors.red[600],
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
         ),
-        body: Center(
-          child: Image(image:NetworkImage('https://images.unsplash.com/photo-1541873676-a18131494184?q=80&w=1618&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'))
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Define the action when the button is pressed
-            print('FloatingActionButton clicked');
-          },
-          child: Icon(Icons.add), 
-          backgroundColor: Colors.red[600],
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(40.0, 40.0, 30.0, 0.0),
+          child: Column(
+            children: <Widget>[
+              
+            ],
+          ),
         ),
       ),
     );
