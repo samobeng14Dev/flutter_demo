7fde6a93f4b26e259e5c48609cb59eb4bc30f5b7
samobeng14Dev <obengsamuel270@gmail.com>
2024-10-18 11:19:37 +0000

    added an avatar

--- time_finder/time_finder_app/lib/main.dart
+++ time_finder/time_finder_app/lib/main.dart
@@ -6,7 +6,7 @@ void main() {
   runApp(
     DevicePreview(
       enabled: !kReleaseMode,
-      builder: (context) => Home(),
+      builder: (context) => const Home(),
     ),
   );
 }
@@ -19,7 +19,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int ninjaLevel=0;
+  int ninjaLevel = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -32,65 +32,73 @@ class _HomeState extends State<Home> {
           backgroundColor: Colors.grey[850],
           elevation: 0.0,
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              ninjaLevel += 1;
+            });
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.grey[800],
+        ),
         body: Padding(
           padding: EdgeInsets.fromLTRB(40.0, 40.0, 30.0, 0.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start, // Ensure all content is aligned left
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Center(
                 child: CircleAvatar(
-                  backgroundImage: AssetImage('ninja.png'),
+                  backgroundImage: AssetImage('assets/images/ninja.png'),
                   radius: 40.0,
                 ),
               ),
               Divider(
                 height: 90.0,
-                color:Colors.grey[800]
+                color: Colors.grey[800],
               ),
               Text(
                 'NAME',
                 style: TextStyle(color: Colors.grey, letterSpacing: 1.0),
               ),
-              SizedBox(
-                height: 10.0,
-              ),
+              SizedBox(height: 10.0),
               Text(
                 'Kwame Afriyie',
                 style: TextStyle(
-                    color: Colors.amberAccent[200],
-                    letterSpacing: 1.0,
-                    fontWeight: FontWeight.bold),
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 1.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              SizedBox(
-                height: 10.0,
-              ),
+              SizedBox(height: 10.0),
               Text(
                 'CURRENT LEVEL',
                 style: TextStyle(color: Colors.grey, letterSpacing: 1.0),
               ),
-              SizedBox(
-                height: 10.0,
-              ),
+              SizedBox(height: 10.0),
               Row(
-                mainAxisAlignment: MainAxisAlignment.start, // Align '8' to the left
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Text(
                     '$ninjaLevel',
                     style: TextStyle(
-                        color: Colors.amberAccent[200],
-                        letterSpacing: 1.0,
-                        fontWeight: FontWeight.bold),
+                      color: Colors.amberAccent[200],
+                      letterSpacing: 1.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ],
               ),
-              SizedBox(
-                height: 30.0,
+              SizedBox(height: 30.0),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.email, color: Colors.grey[400]),
+                  SizedBox(width: 10.0),
+                  Text(
+                    'kwameafriyie@gmail.com',
+                    style: TextStyle(color: Colors.grey[400]),
+                  ),
+                ],
               ),
-              Row( children: <Widget>[
-                Icon(Icons.email, color: Colors.grey[400],),
-                SizedBox(width: 10.0,),
-                Text('kwameafriyie@gmail.com', style: TextStyle(color: Colors.grey[400]),)
-              ],)
             ],
           ),
         ),
@@ -98,5 +106,3 @@ class _HomeState extends State<Home> {
     );
   }
 }
-
-
